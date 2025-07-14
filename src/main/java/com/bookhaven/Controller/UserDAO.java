fea342d418e628f7966d996e4000abd19bd674be
Sajib Sarkar <sajibsarkar551@gmail.com>
2025-07-14 12:29:05 +0600

    progress on the UserDAO, set up DatabaseManager, checking field condiotions and more

--- src/main/java/com/bookhaven/Controller/UserDAO.java
+++ src/main/java/com/bookhaven/Controller/UserDAO.java
@@ -1,5 +1,30 @@
 package com.bookhaven.Controller;
 
+import com.bookhaven.Model.User;
+import com.bookhaven.DataAccessLayer.DatabaseManager;
+
+import java.sql.SQLException;
+
 public class UserDAO {
+
+    private boolean createUser(User User){
+        //this is bullshit
+        return true;
+    }
+
+    private boolean checkByMail(String email){
+        try{
+            DatabaseManager.getConnection();
+        } catch (SQLException e){
+
+            // should be printed in a message window
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+
+        }
+
+
+        return true;
+    }
 
 }
--- src/main/java/com/bookhaven/Model/User.java
+++ src/main/java/com/bookhaven/Model/User.java
@@ -1,4 +1,15 @@
 package com.bookhaven.Model;
 
 public class User {
+    private String firstName;
+    private String secondName;
+    private String email;
+    private char[] password;
+
+    public User(String firstName, String secondName, String email, char[] password){
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.email = email;
+        this.password = password;
+    }
 }
